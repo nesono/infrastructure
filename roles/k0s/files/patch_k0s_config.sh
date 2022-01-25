3e3a3006e5f0e1405866d2a9ea7d9822254aa21e
Jochen Issing <c.333+github@nesono.com>
2022-01-25 22:43:27 +0100

    Fix Ansible provisioning of k0s

--- roles/k0s/files/patch_k0s_config.sh
+++ roles/k0s/files/patch_k0s_config.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 # safer scripts
+# only enable here because the read above seems to fail this setting
 set -euo pipefail
 
-IFS='' read -r -d '' HELM_CONFIG <<"EOF"
+# this is a weird workaround for mktemp. Otherwise yq cannot open the files
+readonly tmpdir=/root/k0s_install_$$
+mkdir -p "$tmpdir"
+
+readonly k0s_config_file='/etc/k0s/k0s.yaml'
+readonly tmp_config_file="$tmpdir/k0s.yaml"
+readonly tmp_helm_chart="$tmpdir/helm_chart.yaml"
+cat <<EOF > $tmp_helm_chart
 extensions:
   helm:
     repositories:
@@ -27,3 +35,10 @@ extensions:
             addresses:
             - 5.9.198.112/29
 EOF
+# workaround because yq cannot acces the original file in /etc/
+cp $k0s_config_file $tmp_config_file
+# workaround because yq has problems with absolute file paths
+cd $tmpdir
+yq eval-all 'select(fileIndex==0).spec.extensions.helm = select(fileIndex==1).extensions.helm | select(fileIndex==0)' -i $tmp_config_file $tmp_helm_chart
+
+mv ${tmp_config_file} $k0s_config_file

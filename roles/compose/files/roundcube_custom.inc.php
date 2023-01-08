fa8ddbd3c44a49feddb8e109f931d7695d6902e6
Jochen Issing <c.333+git@nesono.com>
2023-01-08 18:01:46 +0100

    Activating quota support

--- roles/compose/files/roundcube_custom.inc.php
+++ roles/compose/files/roundcube_custom.inc.php
@@ -1,3 +1,12 @@
 <?php
     $config['managesieve_host'] = 'tls://sieve.nesono.com';
     $config['sent_mbox'] = 'Sent Messages';
+    $config['used_quotas'] = 'YES';
+    $config['quota'] = 'YES';
+    $config['new_quota_table'] = 'YES';
+    // system error reporting, sum of: 1 = log; 4 = show
+    //$config['debug_level'] = 4;
+    //$config['sql_debug'] = true;
+    //$config['imap_debug'] = true;
+    //$config['ldap_debug'] = true;
+    //$config['smtp_debug'] = true;
